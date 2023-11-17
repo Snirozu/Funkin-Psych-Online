1d42f6b54367197348181d07a979e07fbc720953
Snirozu <snirozu788@gmail.com>
2023-11-17 21:28:02 +0100

    dum

--- source/online/Waiter.hx
+++ source/online/Waiter.hx
@@ -5,6 +5,7 @@ import flixel.FlxBasic;
 //thread safe function handler
 class Waiter extends FlxBasic {
     public static var queue:Array<Void->Void> = [];
+    var _queueItem:Void->Void;
 
 	public static function put(func:Void->Void) {
         queue.push(func);
@@ -13,10 +14,12 @@ class Waiter extends FlxBasic {
     override function update(elapsed) {
         super.update(elapsed);
 
-        for (_ in queue) {
-			if (queue.shift() != null) {
-				_();
-            };
+		while (queue.length > 0) {
+			_queueItem = queue.shift();
+            
+			if (_queueItem != null) {
+				_queueItem();
+            }
         }
     }
 }
