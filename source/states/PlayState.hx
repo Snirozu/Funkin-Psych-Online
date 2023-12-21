87fcd712ca78b6e2c74302d274dce56dd968f842
Snirozu <snirozu788@gmail.com>
2023-12-21 13:20:34 +0100

    fixes

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -3924,6 +3924,9 @@ class PlayState extends MusicBeatState
 		});
 
 		GameClient.room.onMessage("custom", function(message:Array<Dynamic>) {
+			if (message.length != 2)
+				return;
+
 			Waiter.put(() -> {
 				callOnLuas('onMessage', message);
 				callOnHScript('onMessage', message);
