c0d391135efae7436d4450ad7a6a2f0ef4345f73
Snirozu <72814880+Snirozu@users.noreply.github.com>
2024-03-22 14:46:11 +0100

    wow

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -17,7 +17,7 @@ import options.OptionsState;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var psychOnlineVersion:String = "0.5.0";
+	public static var psychOnlineVersion:String = "0.6.0";
 	public static var psychEngineVersion:String = '0.7.1h'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 
