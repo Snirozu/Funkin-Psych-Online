99a85581407c125fdd3318f92c30e9c6bef46a29
Meow <103583353+xenkap@users.noreply.github.com>
2024-03-27 00:35:32 +0800

    how did i miss this
    I AM SO SORRY

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -3814,7 +3814,7 @@ class PlayState extends MusicBeatState
 		}
 
 		if (curBeat % dad.danceEveryNumBeats == 0 && dad.animation.curAnim != null && !dad.animation.curAnim.name.startsWith('sing') && !dad.stunned) {
-			sendDance = PlayState.isCharacterPlayer(dad) && dad.animation.curAnim.name.startsWith('idle') && dadMap.animation.curAnim.name.startsWith('dance');
+			sendDance = PlayState.isCharacterPlayer(dad) && dad.animation.curAnim.name.startsWith('idle') && dad.animation.curAnim.name.startsWith('dance');
 			dad.dance();
 		}
 
