c9fea11d70079b009a62170e9c1946e1268566ee
Meow <103583353+xenkap@users.noreply.github.com>
2024-03-27 07:55:34 +0800

    Revert "re-add wacky implementation..."
    This reverts commit 4e27a5e6bcc78620042a6773df6045ccadd7c6c8.

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -2501,8 +2501,6 @@ class PlayState extends MusicBeatState
 					boyfriend.playAnim('hey', true);
 					boyfriend.specialAnim = true;
 					boyfriend.heyTimer = flValue2;
-					if (GameClient.isConnected() && PlayState.isCharacterPlayer(boyfriend))
-						GameClient.send("charPlay", [value1, false, true]);
 				}
 
 			case 'Set GF Speed':
@@ -2538,8 +2536,6 @@ class PlayState extends MusicBeatState
 				{
 					char.playAnim(value1, true);
 					char.specialAnim = true;
-					if (GameClient.isConnected() && PlayState.isCharacterPlayer(char))
-						GameClient.send("charPlay", [value1, char == gf, true]);
 				}
 
 			case 'Camera Follow Pos':
