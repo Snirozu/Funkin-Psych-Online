c54dca2a8a0acff5fd7538919f4543bbbaf4c474
Snirozu <72814880+Snirozu@users.noreply.github.com>
2024-09-22 12:53:04 +0200

    i think its done

--- source/Main.hx
+++ source/Main.hx
@@ -50,7 +50,7 @@ class Main extends Sprite
 	public static var stage3D:AwayStage;
 	#end
 
-	public static final PSYCH_ONLINE_VERSION:String = "0.7.7";
+	public static final PSYCH_ONLINE_VERSION:String = "0.7.8";
 	public static final CLIENT_PROTOCOL:Float = 3;
 	public static final GIT_COMMIT:String = online.Macros.getGitCommitHash();
 
