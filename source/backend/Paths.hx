c5afe14c9f82b6f5ec9e73db7b6954b75766d038
Lily <egzozu.be.bas@gmail.com>
2024-10-03 17:57:19 +0300

    add try catch on returnSound (#58)
    * Update Paths.hx
    
    * Update GameOverSubstate.hx
    
    * b
    
    * Update Paths.hx
    
    * my bad

--- source/backend/Paths.hx
+++ source/backend/Paths.hx
@@ -419,8 +419,14 @@ class Paths
 		#if MODS_ALLOWED
 		var file:String = modsSounds(path, key);
 		if(FileSystem.exists(file)) {
-			if(!currentTrackedSounds.exists(file)) {
-				currentTrackedSounds.set(file, Sound.fromFile(file));
+			try {
+				if(!currentTrackedSounds.exists(file)) {
+					currentTrackedSounds.set(file, Sound.fromFile(file));
+				}
+			} catch (e:Dynamic) {
+				if (ClientPrefs.isDebug())
+					Sys.println('Paths.returnSound(): SOUND NOT FOUND: $key');
+				return null;
 			}
 			localTrackedAssets.push(key);
 			return currentTrackedSounds.get(file);
@@ -430,17 +436,23 @@ class Paths
 		var gottenPath:String = getPath('$path/$key.$SOUND_EXT', SOUND, library);
 		gottenPath = gottenPath.substring(gottenPath.indexOf(':') + 1, gottenPath.length);
 		// trace(gottenPath);
-		if(!currentTrackedSounds.exists(gottenPath))
-		#if MODS_ALLOWED
-			currentTrackedSounds.set(gottenPath, Sound.fromFile('./' + gottenPath));
-		#else
-		{
-			var folder:String = '';
-			if(path == 'songs') folder = 'songs:';
-
-			currentTrackedSounds.set(gottenPath, OpenFlAssets.getSound(folder + getPath('$path/$key.$SOUND_EXT', SOUND, library)));
+		try {
+			if(!currentTrackedSounds.exists(gottenPath))
+			#if MODS_ALLOWED
+				currentTrackedSounds.set(gottenPath, Sound.fromFile(#if !mobile './' + #end gottenPath));
+			#else
+			{
+				var folder:String = '';
+				if(path == 'songs') folder = 'songs:';
+		
+				currentTrackedSounds.set(gottenPath, OpenFlAssets.getSound(folder + getPath('$path/$key.$SOUND_EXT', SOUND, library)));
+			}
+			#end
+		} catch (e:Dynamic) {
+			if (ClientPrefs.isDebug())
+				Sys.println('Paths.returnSound(): SOUND NOT FOUND: $key');
+			return null;
 		}
-		#end
 		localTrackedAssets.push(gottenPath);
 		return currentTrackedSounds.get(gottenPath);
 	}
