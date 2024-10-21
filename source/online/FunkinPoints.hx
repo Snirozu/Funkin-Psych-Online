b75c5a4828ca7eea9de8b10ee92f68e3e181f35f
Snirozu <72814880+Snirozu@users.noreply.github.com>
2024-10-21 21:15:14 +0200

    points pre-v3

--- source/online/FunkinPoints.hx
+++ source/online/FunkinPoints.hx
@@ -2,27 +2,22 @@ package online;
 
 @:build(online.backend.Macros.getSetForwarder())
 class FunkinPoints {
-	@:forwardField(FlxG.save.data.funkinPointsv2, 0)
+	@:forwardField(FlxG.save.data.funkinPointsv3, 0)
 	public static var funkinPoints(get, set):Float;
 
-    public static function calcFP(accuracy:Float, misses:Float, noteDensity:Float, notesHit:Float, combo:Float, playbackRate:Float, songSpeed:Float):Float {
-        if (accuracy <= 0)
-            return 0;
-		if (notesHit <= 0)
+    public static function calcFP(accuracy:Float, misses:Float, denseNotes:Float, notesHit:Float, maxCombo:Float, playbackRate:Float):Float {
+		if (accuracy <= 0 || notesHit <= 0)
             return 0;
 
-		var fp:Float = notesHit / (noteDensity / 2);
-		fp *= 1 + combo / 1000;
-		fp *= accuracy / (misses * 0.2 + 1) * (1 - noteDensity / 500);
-		fp *= playbackRate;
-		var songSpeedAdd = songSpeed - 3.0;
-		if (songSpeedAdd > 0)
-			fp *= 1 + songSpeedAdd;
+		// depends on player's hitted notes, and their weight
+		var fp:Float = Math.max(1, 1 + denseNotes / 800) * (notesHit / 100);
+		fp *= 1 + maxCombo / 1000; // depends on player's note streak
+		fp *= accuracy / (1 + misses * 0.25); // depends on player's note accuracy
 		return Math.ffloor(fp);
     }
 
-	public static function save(accuracy:Float, misses:Float, noteDensity:Float, notesHit:Float, combo:Float, playbackRate:Float, songSpeed:Float) {
-		var gained:Float = online.FunkinPoints.calcFP(accuracy, misses, noteDensity, notesHit, combo, playbackRate, songSpeed);
+	public static function save(accuracy:Float, misses:Float, denseNotes:Float, notesHit:Float, maxCombo:Float, playbackRate:Float) {
+		var gained:Float = online.FunkinPoints.calcFP(accuracy, misses, denseNotes, notesHit, maxCombo, playbackRate);
 		funkinPoints += gained;
 		FlxG.save.flush();
 		GameClient.send("updateFP", funkinPoints);
