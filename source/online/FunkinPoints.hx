10ecfbfff9e973b64681fdae27f2149100163a12
Snirozu <72814880+Snirozu@users.noreply.github.com>
2024-11-18 19:08:19 +0100

    fp don

--- source/online/FunkinPoints.hx
+++ source/online/FunkinPoints.hx
@@ -5,52 +5,27 @@ class FunkinPoints {
 	@:forwardField(FlxG.save.data.funkinPointsv4, 0)
 	public static var funkinPoints(get, set):Float;
 
-    public static function calcFP(accuracy:Float, misses:Float, denseNotes:Float, notesHit:Float, maxCombo:Float):Float {
+    public static function calcFP(accuracy:Float, misses:Float, songDensity:Float, notesHit:Float, maxCombo:Float):Float {
 		if (accuracy <= 0 || notesHit <= 0)
             return 0;
 
 
-		// depends on the amount of hitted notes (1fp per 5-150 notes)
-		var fp:Float = notesHit / (denseNotes / 2);
-		// depends on player's note streak (x2fp per 1000 combo)
-		fp *= 1 + maxCombo / 1000;
+		// depends on the amount of hitted notes and the density of the song
+		// density values for songs: 2.9p (unbeatable), 3.6 (ballistic), 4.2 (spookeez erect), 4.6 (sporting) 
+		// so for a song with 3.0 density (ex. spookeez erect), for every ~65 hitted notes a player will gain 1 fp (without combo bonus)
+		var fp:Float = (1 + songDensity) * (notesHit / 200);
+		// depends on player's note streak (x2fp per 2000 combo)
+		fp *= 1 + maxCombo / 2000;
 		// depends on player's note accuracy (weighted by power of 3; 95% = x0.85, 90% = x0.72, 80% = x0.512)
 		fp *= Math.pow(accuracy, 3) / (1 + misses * 0.25);
 		return Math.ffloor(fp);
     }
 
-	public static function save(accuracy:Float, misses:Float, denseNotes:Float, notesHit:Float, maxCombo:Float) {
-		var gained:Float = online.FunkinPoints.calcFP(accuracy, misses, denseNotes, notesHit, maxCombo);
+	public static function save(accuracy:Float, misses:Float, songDensity:Float, notesHit:Float, maxCombo:Float) {
+		var gained:Float = online.FunkinPoints.calcFP(accuracy, misses, songDensity, notesHit, maxCombo);
 		funkinPoints += gained;
 		FlxG.save.flush();
 		GameClient.send("updateFP", funkinPoints);
 		return gained;
     }
 }
-
-// @:build(online.backend.Macros.getSetForwarder())
-// class FunkinPoints {
-// 	@:forwardField(FlxG.save.data.funkinPointsv4, 0)
-// 	public static var funkinPoints(get, set):Float;
-
-// 	public static function calcFP(accuracy:Float, misses:Float, notesHit:Float, maxCombo:Float):Float {
-// 		if (accuracy <= 0 || notesHit <= 0)
-// 			return 0;
-
-// 		// depends on player's hitted notes (1fp per 100 hitted notes)
-// 		var fp:Float = notesHit / 100;
-// 		// depends on player's note streak (x2fp per 500 combo)
-// 		fp *= 1 + maxCombo / 500;
-// 		// depends on player's note accuracy (weighted by power of 3; 95% = x0.85, 90% = x0.72, 80% = x0.512)
-// 		fp *= Math.pow(accuracy, 3) / (1 + misses * 0.25);
-// 		return Math.ffloor(fp);
-// 	}
-
-// 	public static function save(accuracy:Float, misses:Float, notesHit:Float, maxCombo:Float) {
-// 		var gained:Float = online.FunkinPoints.calcFP(accuracy, misses, notesHit, maxCombo);
-// 		funkinPoints += gained;
-// 		FlxG.save.flush();
-// 		GameClient.send("updateFP", funkinPoints);
-// 		return gained;
-// 	}
-// }
