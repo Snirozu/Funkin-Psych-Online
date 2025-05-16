c2e0a524e73e82e900348a02f51a09a2b2cd1edd
Snirozu <72814880+Snirozu@users.noreply.github.com>
2025-05-16 21:50:34 +0200

    Update source/objects/Character.hx
    Co-authored-by: Til <89487150+TechnikTil@users.noreply.github.com>

--- source/objects/Character.hx
+++ source/objects/Character.hx
@@ -147,7 +147,6 @@ class Character extends FlxSprite {
 		curCharacter = character;
 		this.isPlayer = isPlayer;
 		this.isSkin = isSkin;
-		this.Custom = isSkin;
 		var library:String = null;
 		switch (curCharacter) {
 			// case 'your character name in case you want to hardcode them instead':
