3ad3cf68f0658d42ef1f7a87b6589b1d734e3e9c
Snirozu <72814880+Snirozu@users.noreply.github.com>
2025-05-28 17:51:26 +0200

    ohhh

--- source/Main.hx
+++ source/Main.hx
@@ -44,7 +44,7 @@ class Main extends Sprite
 
 	public static var fpsVar:FPS;
 
-	public static final PSYCH_ONLINE_VERSION:String = "0.11.8";
+	public static final PSYCH_ONLINE_VERSION:String = "0.11.9";
 	public static final CLIENT_PROTOCOL:Float = 8;
 	public static final GIT_COMMIT:String = online.backend.Macros.getGitCommitHash();
 	public static final LOW_STORAGE:Bool = online.backend.Macros.hasNoCapacity();
