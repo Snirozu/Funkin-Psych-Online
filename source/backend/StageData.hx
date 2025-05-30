afd40b5d973507141e92420de02cd8c5d2d3a103
TechnikTil <til.tjardes@gmail.com>
2025-05-30 17:42:16 -0600

    add 1.0 stage support + improve hscript scripting compatibility

--- source/backend/StageData.hx
+++ source/backend/StageData.hx
@@ -1,5 +1,6 @@
 package backend;
 
+import psychlua.ModchartSprite;
 import json2object.JsonParser;
 #if MODS_ALLOWED
 import sys.io.File;
@@ -28,6 +29,19 @@ typedef StageFile = {
 
 	@:optional var characterFeetPos:Bool; // vslice character positioning
 	@:optional var stage3D:online.away.DataProps.StageData3D;
+
+	// 1.0 stuffs
+	@:optional var preload:Dynamic;
+	@:optional var objects:Array<Dynamic>;
+}
+
+enum abstract LoadFilters(Int) from Int from UInt to Int to UInt
+{
+	var LOW_QUALITY:Int = (1 << 0);
+	var HIGH_QUALITY:Int = (1 << 1);
+
+	var STORY_MODE:Int = (1 << 2);
+	var FREEPLAY:Int = (1 << 3);
 }
 
 class StageData {
@@ -110,7 +124,7 @@ class StageData {
 		{
 			return null;
 		}
-		return new JsonParser<StageFile>().fromJson(rawJson) ?? cast Json.parse(rawJson);
+		return /*new JsonParser<StageFile>().fromJson(rawJson) ?? */cast Json.parse(rawJson);
 	}
 
 	public static function vanillaSongStage(songName):String
@@ -135,5 +149,117 @@ class StageData {
 				return 'tank';
 		}
 		return 'stage';
+	}
+
+	// PSYCH ENGINE 1.0 STUFFS
+	public static var reservedNames:Array<String> = ['gf', 'gfGroup', 'dad', 'dadGroup', 'boyfriend', 'boyfriendGroup']; //blocks these names from being used on stage editor's name input text
+	public static function addObjectsToState(objectList:Array<Dynamic>, gf:FlxSprite, dad:FlxSprite, boyfriend:FlxSprite, ?group:Dynamic = null, ?ignoreFilters:Bool = false)
+	{
+		var addedObjects:Map<String, FlxSprite> = [];
+		for (num => data in objectList)
+		{
+			if (addedObjects.exists(data)) continue;
+
+			switch(data.type)
+			{
+				case 'gf', 'gfGroup':
+					if(gf != null)
+					{
+						gf.ID = num; 
+						if (group != null) group.add(gf);
+						addedObjects.set('gf', gf);
+					}
+				case 'dad', 'dadGroup':
+					if(dad != null)
+					{
+						dad.ID = num;
+						if (group != null) group.add(dad);
+						addedObjects.set('dad', dad);
+					}
+				case 'boyfriend', 'boyfriendGroup':
+					if(boyfriend != null)
+					{
+						boyfriend.ID = num;
+						if (group != null) group.add(boyfriend);
+						addedObjects.set('boyfriend', boyfriend);
+					}
+
+				case 'square', 'sprite', 'animatedSprite':
+					if(!ignoreFilters && !validateVisibility(data.filters)) continue;
+
+					var spr:ModchartSprite = new ModchartSprite(data.x, data.y);
+					spr.ID = num;
+					if(data.type != 'square')
+					{
+						if(data.type == 'sprite')
+							spr.loadGraphic(Paths.image(data.image));
+						else
+							spr.frames = Paths.getAtlas(data.image);
+						
+						if(data.type == 'animatedSprite' && data.animations != null)
+						{
+							var anims:Array<objects.Character.AnimArray> = cast data.animations;
+							for (key => anim in anims)
+							{
+								if(anim.indices == null || anim.indices.length < 1)
+									spr.animation.addByPrefix(anim.anim, anim.name, anim.fps, anim.loop);
+								else
+									spr.animation.addByIndices(anim.anim, anim.name, anim.indices, '', anim.fps, anim.loop);
+	
+								if(anim.offsets != null)
+									spr.addOffset(anim.anim, anim.offsets[0], anim.offsets[1]);
+	
+								if(spr.animation.curAnim == null || data.firstAnimation == anim.anim)
+									spr.playAnim(anim.anim, true);
+							}
+						}
+						for (varName in ['antialiasing', 'flipX', 'flipY'])
+						{
+							var dat:Dynamic = Reflect.getProperty(data, varName);
+							if(dat != null) Reflect.setProperty(spr, varName, dat);
+						}
+						if(!ClientPrefs.data.antialiasing) spr.antialiasing = false;
+					}
+					else
+					{
+						spr.makeGraphic(1, 1, FlxColor.WHITE);
+						spr.antialiasing = false;
+					}
+
+					if(data.scale != null && (data.scale[0] != 1.0 || data.scale[1] != 1.0))
+					{
+						spr.scale.set(data.scale[0], data.scale[1]);
+						spr.updateHitbox();
+					}
+					spr.scrollFactor.set(data.scroll[0], data.scroll[1]);
+					spr.color = CoolUtil.colorFromString(data.color);
+					
+					for (varName in ['alpha', 'angle'])
+					{
+						var dat:Dynamic = Reflect.getProperty(data, varName);
+						if(dat != null) Reflect.setProperty(spr, varName, dat);
+					}
+
+					if (group != null) group.add(spr);
+					addedObjects.set(data.name, spr);
+
+				default:
+					var err = '[Stage .JSON file] Unknown sprite type detected: ${data.type}';
+					trace(err);
+					FlxG.log.error(err);
+			}
+		}
+		return addedObjects;
+	}
+
+	public static function validateVisibility(filters:LoadFilters)
+	{
+		if((filters & STORY_MODE) == STORY_MODE)
+			if(!PlayState.isStoryMode) return false;
+		else if((filters & FREEPLAY) == FREEPLAY)
+			if(PlayState.isStoryMode) return false;
+
+		return ((ClientPrefs.data.lowQuality && (filters & LOW_QUALITY) == LOW_QUALITY) ||
+			(!ClientPrefs.data.lowQuality && (filters & HIGH_QUALITY) == HIGH_QUALITY));
 	}
 }
