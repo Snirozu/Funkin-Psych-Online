e34ca623d02b4fc43fcaac34856251a9e11a31df
DrEmiliano02 <dremiliano02@gmail.com>
2025-09-04 17:19:12 -0600

    cffrjwekfbdsa

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -189,8 +189,8 @@ class FreeplayState extends MusicBeatState
 		FlxG.mouse.visible = false;
 		FlxG.autoPause = false;
 
-		Paths.clearStoredMemory();
-		//Paths.clearUnusedMemory();
+		//Paths.clearStoredMemory();
+		Paths.clearUnusedMemory();
 		
 		persistentUpdate = true;
 		PlayState.isStoryMode = false;
