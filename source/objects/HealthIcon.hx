42620d26ea3a70475f47b4a1a3395f0d2a57dcb6
DrEmiliano02 <dremiliano02@gmail.com>
2025-09-04 13:20:02 -0600

    shhh

--- source/objects/HealthIcon.hx
+++ source/objects/HealthIcon.hx
@@ -3,13 +3,15 @@ package objects;
 class HealthIcon extends FlxSprite
 {
 	public var sprTracker:FlxSprite;
-	private var isPlayer:Bool = false;
-	public var hasWinning:Bool = true;
+	private var isOldIcon:Bool = false;
+	public var isPlayer:Bool = false;
 	private var char:String = '';
+	public var ox:Int;
 
-	public function new(char:String = 'face', isPlayer:Bool = false, ?allowGPU:Bool = true)
+	public function new(char:String = 'bf', isPlayer:Bool = false, ?allowGPU:Bool = true)
 	{
 		super();
+		isOldIcon = (char == 'bf-old');
 		this.isPlayer = isPlayer;
 		changeIcon(char, allowGPU);
 		scrollFactor.set();
@@ -26,29 +28,20 @@ class HealthIcon extends FlxSprite
 	private var iconOffsets:Array<Float> = [0, 0];
 	public function changeIcon(char:String, ?allowGPU:Bool = true) {
 		if(this.char != char) {
-			var hasWin:Bool = false;
-			var singleIcon:Bool = false;
-
 			var name:String = 'icons/' + char;
 			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-' + char; //Older versions of psych engine's support
 			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-face'; //Prevents crash from missing icon
-
+			
 			var graphic = Paths.image(name, allowGPU);
-			if (graphic.width == 450) hasWin = true;
-			else if (graphic.width == 150) singleIcon = true;
 			var iSize:Float = Math.round(graphic.width / graphic.height);
 			loadGraphic(graphic, true, Math.floor(graphic.width / iSize), Math.floor(graphic.height));
 			iconOffsets[0] = (width - 150) / iSize;
-			loadGraphic(graphic, true, Math.floor(graphic.width / (hasWin ? 3 : (singleIcon ? 1 : iSize))), Math.floor(graphic.height));
-			iconOffsets[0] = (width - 150) / (hasWin ? 3 : (singleIcon ? 1 : iSize));
 			iconOffsets[1] = (height - 150) / iSize;
 			updateHitbox();
 
 			animation.add(char, [for(i in 0...frames.frames.length) i], 0, false, isPlayer);
-			animation.add(char, [0, 1, 2], 0, false, isPlayer);
 			animation.play(char);
 			this.char = char;
-			this.hasWinning = hasWin;
 
 			if(char.endsWith('-pixel'))
 				antialiasing = false;
@@ -57,15 +50,11 @@ class HealthIcon extends FlxSprite
 		}
 	}
 
-	public var autoAdjustOffset:Bool = true;
 	override function updateHitbox()
 	{
 		super.updateHitbox();
-		if(autoAdjustOffset)
-		{
-			offset.x = iconOffsets[0];
-			offset.y = iconOffsets[1];
-		}
+		offset.x = iconOffsets[0];
+		offset.y = iconOffsets[1];
 	}
 
 	public function getCharacter():String {
